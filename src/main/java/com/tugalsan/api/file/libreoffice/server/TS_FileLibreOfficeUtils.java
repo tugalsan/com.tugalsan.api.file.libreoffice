f2041cbc79a2599b2364801963a13a2b36643011
tugalsan karabacak <tugalsan@users.noreply.github.com>
2025-03-24 10:59:54 +0300

    Initial commit

---
+++ src/main/java/com/tugalsan/api/file/libreoffice/server/TS_FileLibreOfficeUtils.java
@@ -0,0 +1,13 @@
+package com.tugalsan.api.file.libreoffice.server;
+
+import com.tugalsan.api.log.server.TS_Log;
+
+public class TS_FileLibreOfficeUtils {
+
+    private TS_FileLibreOfficeUtils() {
+
+    }
+
+    final private static TS_Log d = TS_Log.of(TS_FileLibreOfficeUtils.class);
+
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,4 @@
+module com.tugalsan.api.file.libreoffice {
+    requires com.tugalsan.api.log;
+    exports com.tugalsan.api.file.libreoffice.server;
+}
