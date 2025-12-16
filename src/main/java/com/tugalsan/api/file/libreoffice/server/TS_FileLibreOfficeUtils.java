9e62ca95e3c0883d1dc5999d488b6df4ea4edc8f
tugalsan karabacak <tugalsan@users.noreply.github.com>
2025-12-16 12:34:56 +0300

    .

--- src/main/java/com/tugalsan/api/file/libreoffice/server/TS_FileLibreOfficeUtils.java
+++ src/main/java/com/tugalsan/api/file/libreoffice/server/TS_FileLibreOfficeUtils.java
@@ -1,5 +1,10 @@
 package com.tugalsan.api.file.libreoffice.server;
 
+import module com.tugalsan.api.function;
+import module com.tugalsan.api.union;
+import java.nio.file.Path;
+import org.jodconverter.local.*;
+
 import module com.tugalsan.api.log;
 
 public class TS_FileLibreOfficeUtils {
@@ -10,4 +15,10 @@ public class TS_FileLibreOfficeUtils {
 
     final private static TS_Log d = TS_Log.of(TS_FileLibreOfficeUtils.class);
 
+    public static TGS_UnionExcuseVoid convert(Path input, Path output) {
+        return TGS_FuncMTCUtils.call(() -> {
+            JodConverter.convert(input.toFile()).to(output.toFile()).execute();
+            return TGS_UnionExcuseVoid.ofVoid();
+        }, e -> TGS_UnionExcuseVoid.ofExcuse(e));
+    }
 }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,8 +1,13 @@
 module com.tugalsan.api.file.libreoffice {
     requires com.tugalsan.api.log;
-    requires juh;
-    requires jurt;
-    requires ridl;
-    requires unoil;
+    requires com.tugalsan.api.function;
+    requires com.tugalsan.api.union;
+    requires jodconverter.core;
+    requires jodconverter.local;
+    requires jodconverter.local.lo;
+//    requires juh;
+//    requires jurt;
+//    requires ridl;
+//    requires unoil;
     exports com.tugalsan.api.file.libreoffice.server;
 }
